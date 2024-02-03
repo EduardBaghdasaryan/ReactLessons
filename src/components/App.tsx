8414e1d729dec393542d7806fc81be3a10c4f954
Eduard Baghdasaryan <eduardbadghasaryan1995@gmail.com>
2024-02-03 15:17:41 +0400

    First lesson(webpack)

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const LOADERS = [
+  {
+    test: /\.(ts|tsx)$/,
+    exclude: /node_modules/,
+    use: "babel-loader",
+  },
+];
+
+const PLUGINS = [
+  new HtmlWebPackPlugin({
+    template: "./src/index.html",
+  }),
+];
+
+module.exports = {
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: LOADERS,
+  },
+  plugins: PLUGINS,
+  devServer: {
+    port: 3002,
+  },
+};
