92929988cc4757fb9fe98f67d7d2824d74839c67
Eduard <eduard@Vahagns-MacBook-Pro.local>
2024-03-15 15:36:43 +0400

    Refacctore routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,9 @@
 import React, { FC } from "react";
-import SignIn from "./signin/Signin";
-
 import { useSelector } from "react-redux";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ROUTES } from "../constants/index";
 import { isAuthSelector } from "../store/signin/signin-selector";
+import SignIn from "./signin/Signin";
 import Signup from "./signin/Signup";
 import Home from "./home/Home";
 import MyProfile from "./profile/MyProfile";
@@ -16,25 +15,23 @@ import CreateProduct from "./admin/AddProduct";
 
 const App: FC = () => {
   const isAuth = useSelector(isAuthSelector);
+
   return (
     <Router>
       <Routes>
         <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
         <Route path={ROUTES.SIGN_UP} element={<Signup />} />
-        {isAuth && <Route path={ROUTES.HOME} element={<Home />} />}
-        {isAuth && <Route path={ROUTES.MY_PROFILE} element={<MyProfile />} />}
         {isAuth && (
-          <Route path={ROUTES.PRODUCT_LIST} element={<ProductsList />} />
+          <>
+            <Route path={ROUTES.HOME} element={<Home />} />
+            <Route path={ROUTES.MY_PROFILE} element={<MyProfile />} />
+            <Route path={ROUTES.PRODUCT_LIST} element={<ProductsList />} />
+            <Route path={ROUTES.PRODUCT_DETAILS} element={<ProductDetails />} />
+            <Route path={ROUTES.SHOPPING_CART} element={<ShoppingList />} />
+            <Route path={ROUTES.ORDERS} element={<OrdersList />} />
+            <Route path={ROUTES.ADMIN} element={<CreateProduct />} />
+          </>
         )}
-        {isAuth && (
-          <Route path={ROUTES.PRODUCT_DETAILS} element={<ProductDetails />} />
-        )}
-        {isAuth && (
-          <Route path={ROUTES.SHOPPING_CART} element={<ShoppingList />} />
-        )}
-        {isAuth && <Route path={ROUTES.ORDERS} element={<OrdersList />} />}
-        <Route path="*" element={<SignIn />} />
-        {isAuth && <Route path={ROUTES.ADMIN} element={<CreateProduct />} />}
         <Route path="*" element={<SignIn />} />
       </Routes>
     </Router>
